28d04d101825665599914c923d570dc0fc0b38e9
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-27 17:25:37 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,36 @@
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        try{
+            Future<Integer> inputNum = executor.submit(getCallableInteger());
+            int outputNum = inputNum.get();
+            System.out.println(outputNum);
+        }catch (InterruptedException | ExecutionException e){
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
-    }
+        try(Scanner scanner = new Scanner(System.in)){
+
+            System.out.println("Please write a integer: ");
+            int inputInteger = scanner.nextInt();
+            return new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    return inputInteger;
+                }
+            };
+
+        }
+
+
+    };
 }
